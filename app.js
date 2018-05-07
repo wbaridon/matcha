9e5df5a39b8ad8751c04a5edee0a27da1f8260a5
Wenceslas BARIDON <wbaridon@e3r2p20.42.fr>
2018-05-07 15:33:27 +0200

    Changement route

--- app.js
+++ app.js
@@ -11,22 +11,11 @@ app.use(session({
 	keys: ['M42atchaW?baridoN'],
 }))
 
-var test = require('./routes/test');
-app.use('/test', test)
+var register = require('./routes/register');
+app.use('/register', register)
 
 app.get('/', function(req, res) {
 	res.render('home.ejs');
-})
-
-.post('/register', urlencodedParser, function (req, res) {
-	console.log('Entre dans register');
-	if (req.body.login == '' || req.body.password == '' && req.body.submit != "S'inscrire")
-		console.log('Vide');
-	else {
-	console.log(req.body.login);
-	console.log(req.body.password);
-}
-	res.redirect('/');
 })
 
 .use(function(req, res, next) {
---
+++ routes/register.js
@@ -0,0 +1,27 @@
+// Test routeur
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+router.get('/', function(req, res) {
+
+	res.render('error.ejs', {error: 'Aucun message'});
+})
+
+router.get('/about', function(req, res) {
+	res.send('About');
+})
+
+router.post('/', urlencodedParser, function (req, res) {
+	console.log('Entre dans register');
+	if (req.body.login == '' || req.body.password == '' && req.body.submit != "S'inscrire")
+		console.log('Vide');
+	else {
+	console.log(req.body.login);
+	console.log(req.body.password);
+}
+	res.redirect('/');
+})
+
+module.exports = router;
