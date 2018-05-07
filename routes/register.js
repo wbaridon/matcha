2bc9c07b8716471354756a3895036a6e1221675f
Wenceslas BARIDON <wbaridon@e3r2p20.42.fr>
2018-05-07 16:10:05 +0200

    password

--- routes/register.js
+++ routes/register.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var argon2 = require('argon2');
 
 router.get('/', function(req, res) {
 
@@ -16,8 +17,11 @@ router.get('/about', function(req, res) {
 router.post('/', urlencodedParser, function (req, res) {
 	console.log('Entre dans register');
 	if (req.body.login == '' || req.body.password == '' && req.body.submit != "S'inscrire")
-		console.log('Vide');
+		res.render('error.ejs', {error: 'Merci de remplir tous les champs'});
 	else {
+		argon2.hash(req.body.password).then(hash => {
+			console.log(hash);
+})
 	console.log(req.body.login);
 	console.log(req.body.password);
 }
