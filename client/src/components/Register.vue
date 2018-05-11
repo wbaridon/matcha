662dc7fae076b6a6966f567517c9e782cd7afcdc
Wenceslas BARIDON <wbaridon@e3r2p20.42.fr>
2018-05-11 18:13:03 +0200

    Mise en place d'un petit front

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
-    <h1>Posts</h1>
-    This fill will list all the posts.
+    <h1>S'inscrire</h1>
+    <form class="register"  action="index.html" method="post">
+      <label for="login">Login</label>
+      <input type="text" name="login" value="">
+      <label for="email">Email </label>
+      <input type="email" name="email" value="">
+      <label for="password">Mot de passe </label>
+      <input type="password" name="password" value="">
+      <label for="firstname">Prenom </label>
+      <input type="text" name="firstname" value="">
+      <label for="name">Nom </label>
+      <input type="text" name="name" value="">
+      <input type="submit" name="submit" value="S'inscrire">
+    </form>
   </div>
 </template>
 
@@ -12,3 +24,25 @@ export default {
   }
 }
 </script>
+
+<style>
+  .register {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2%;
+  }
+  .register input {
+    width: 250px;
+    padding: 5px;
+    margin: 5px;
+  }
+  .register label {
+    font-weight: bold;
+  }
+  .register input[type=submit] {
+    background-color: lightgrey;
+    margin-top: 15px;
+    font-weight: bold;
+  }
+</style>
