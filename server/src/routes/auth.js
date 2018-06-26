dd1d875ae118c6725ddb31a06c8ad06c910b7885
Wenceslas BARIDON <wbaridon@e1r6p17.42.fr>
2018-06-26 14:14:22 +0200

    ok

---
+++ server/src/routes/auth.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var argon2 = require('argon2');
+var model = require('../models/account.js');
+
+router.post('/', function (req, res) {
+
+});
+
+module.exports = router;
--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
-var json = bodyParser.json();
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 var argon2 = require('argon2');
 var model = require('../models/account.js');
 
-router.post('/', json, function (req, res) {
+router.post('/', function (req, res) {
 	console.log(req.body)
 	login = req.body.login
 	password = req.body.password
