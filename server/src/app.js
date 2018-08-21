e997d79a511882bde8556eeb1f3b82c67ae70b1d
Wenceslas BARIDON <wbaridon@e2r8p19.42.fr>
2018-08-21 16:46:30 +0200

    Update

--- server/src/app.js
+++ server/src/app.js
@@ -16,12 +16,12 @@ app.use(cors())
 app.use('/register', require('./routes/register'))
 app.use('/login', require('./routes/login'))
 
-app.get('/posts', (req, res) => {
-  res.send(
+/* app.get('/posts', (req, res) => {
+   res.send(
     [{
       title: "Hello World",
       description: "Hi there!, How are you?"
     }]
   )
-})
+})*/
 app.listen(process.env.PORT || 8081)
