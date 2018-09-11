07548a4f969d196ab1274deed58af48f60ae1c48
Wenceslas BARIDON <wbaridon@e2r8p20.42.fr>
2018-09-11 12:34:21 +0200

    Recuperation des data pour Suggestion Front

--- server/src/routes/suggestion.js
+++ server/src/routes/suggestion.js
@@ -7,8 +7,42 @@ var suggestionList = require('../models/suggestionList.js');
 
 router.post('/', function(req, res) {
 	suggestionList.showList(result => {
-		res.send(result)
+		convertUserData(result, user => {
+			res.send(user)
+		})
 	})
 })
+
+function convertUserData(user, callback) {
+  // synchrone ou asynchrone ?
+	var counter = user.length;
+
+	user.forEach(function (item, index, array) {
+		switch (item.gender) {
+	    case 0:
+	      item.gender = 'Homme';
+	      break;
+	    case 1:
+	      item.gender = "Femme";
+	      break;
+	  }
+		switch (item.sexuality) {
+	    case 0:
+	      item.sexuality = 'Hetero';
+	      break;
+	    case 1:
+	      item.sexuality = 'Homo';
+	      break;
+	    case 2:
+	      item.sexuality = 'Bisexuel';
+	      break;
+	  }
+		counter--
+		if (counter === 0) {
+			callback(array)
+		}
+	})
+
+}
 
 module.exports = router;
