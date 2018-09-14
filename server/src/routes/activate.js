7690dbcf8b1ce6395807892bfb0f882af3958cd7
Sebastien BRUCKERT <seb.bruckert@gmail.com>
2018-09-14 12:34:53 +0200

    Fix issue with bad email in confirmating email

--- server/src/routes/activate.js
+++ server/src/routes/activate.js
@@ -13,10 +13,12 @@ router.post('/', function (req, res) {
         let email = req.body.email;
         let key = req.body.key;
 
-        model.userTimestampFromEmail(email, (err, callback) => { //Mail existing in DB with associated timestamp
+        model.userTimestampFromEmail(email, (err, resp) => { //Mail existing in DB with associated timestamp
             if (err) throw err; 
-            else if (callback[0].activation === 0) {
-                myhash.verify(email + callback[0].timestamp, key, match => {
+            else if (!resp[0])
+                res.send(`This mail ${email} doesn't exists.`);
+            else if (resp[0].activation === 0) {
+                myhash.verify(email + resp[0].timestamp, key, match => {
                     if (match) { //Hash correspondant
                         model.activateAccount(email, (err, result) => {
                             if (err) throw err;
