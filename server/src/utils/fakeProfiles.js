b3b1059a4a0afebbf757dc487306152db54851a8
Sebastien BRUCKERT <seb.bruckert@gmail.com>
2018-09-14 16:19:24 +0200

    Localisation GPS OK

--- server/src/utils/fakeProfiles.js
+++ server/src/utils/fakeProfiles.js
@@ -12,6 +12,12 @@ const AGE_MIN = 18;
 const AGE_MAX = 65;
 const ACTIVATED_ACCOUNT = true;
 const EMAIL = '@matcha.fr';
+const CENTER_X = 2.318397700
+const CENTER_Y = 48.896722700
+const AROUND_X = 4
+const AROUND_Y = 4
+const NBR_SAMPLES = 5
+const NBR_GRADIENTS = 100000
 /******************************************************************************/
 const NBR_OF_INTERESTS = config.interests.length;
 let SQL_INTERESTS = '';
@@ -34,6 +40,15 @@ let getRandomAge = () => {
     return getRandomInt(AGE_MAX - AGE_MIN) + AGE_MIN;
 }
 
+let gradientGen = (callback) => {
+    let generated_number = NBR_GRADIENTS
+    for(let i = 0; i < NBR_SAMPLES; i++)
+        generated_number = getRandomInt(generated_number)
+    if (getRandomInt(0, 2) === 0)
+        generated_number = -generated_number;
+    callback(generated_number);
+}
+
 /******************************************************************************/
 
 let Profile = function() {
@@ -52,6 +67,17 @@ let Profile = function() {
     this.age = getRandomAge();
     this.sexuality = getRandomInt(NBR_OF_SEXUALITY);
     this.idAccount;
+    this.latitude;
+    this.longitude;
+
+    this.setLocation = () => {
+        gradientGen(nbr1 => {
+            this.latitude = CENTER_Y + (nbr1 / NBR_GRADIENTS * AROUND_X)
+            gradientGen(nbr2 => {
+                this.longitude = CENTER_X + (nbr2 / NBR_GRADIENTS * AROUND_X)
+            })
+        })
+    }
 
     this.getSQLAccounts = () => {
         return `(${ACTIVATED_ACCOUNT}, '${this.login}', '${this.email}', ${this.timestamp})`;
@@ -68,7 +94,9 @@ let Profile = function() {
     };
 
     this.getSQLProfiles = () => {
-        return `('${this.idAccount}', '${this.name}', '${this.firstname}', ${this.gender}, ${this.age}, ${this.sexuality})`;
+        this.setLocation()
+        //console.log(this.latitude + " / " + this.longitude);
+        return `('${this.idAccount}', '${this.name}', '${this.firstname}', ${this.gender}, ${this.age}, ${this.sexuality}, ${this.latitude}, ${this.longitude})`;
     };
 
     this.getSQLInterests = (nbrInterests) => {
@@ -117,7 +145,7 @@ let addInDb = (nbr) => {
 let addInDbProfiles = (nbr) => {
     return new Promise((resolve, reject) => {
         let SQLQuery = 'INSERT INTO profiles \
-        (id_account, name, firstname, gender, age, sexuality) \
+        (id_account, name, firstname, gender, age, sexuality, latitude, longitude) \
         VALUES ';
         for (let i = 0; i < nbr; i++) {
             SQLQuery += prfs[i].getSQLProfiles();
