7d9063def293f243e1cd9dab7653315980c2d869
Sebastien BRUCKERT <seb.bruckert@gmail.com>
2018-10-06 15:25:03 +0200

    Localisation OK

--- server/src/utils/fakeProfiles.js
+++ server/src/utils/fakeProfiles.js
@@ -1,9 +1,11 @@
-const mysql = require('mysql');
+const NodeGeocoder = require('node-geocoder');
+const geocoder = NodeGeocoder({ provider: 'locationiq', httpAdapter: 'https', apiKey: '2c29b16aa6aabf' });
 const db = require('../config/db');
 const config = require('../config/config');
 const account = require('../models/account');
 
 /******************************************************************************/
+const ALLOW_GEOCODER = true;
 const LOGIN_RANDOM_INT = 10000;
 const MAX_PROFILES = 1000;
 const NBR_OF_INTERESTS_CHANCE = 3;
@@ -58,7 +60,7 @@ let Profile = function() {
     else
         Profile.count++;
 
-    this.firstname = config.firstNames[getRandomInt(config.firstNames.length)];
+    this.firstname = config.firstNames.men[getRandomInt(config.firstNames.men.length)];
     this.name = config.lastNames[getRandomInt(config.lastNames.length)];
     this.timestamp = Date.now();
     this.login = this.timestamp + '.' + Profile.count + '.' + getRandomInt(LOGIN_RANDOM_INT);
@@ -69,19 +71,34 @@ let Profile = function() {
     this.idAccount;
     this.latitude;
     this.longitude;
+    this.zipCode = 0;
+    this.city = 0;
 
     this.setLocation = () => {
-        gradientGen(nbr1 => {
-            this.latitude = CENTER_Y + (nbr1 / NBR_GRADIENTS * AROUND_X)
-            gradientGen(nbr2 => {
-                this.longitude = CENTER_X + (nbr2 / NBR_GRADIENTS * AROUND_X)
+        return new Promise ((resolve, reject) => {
+            gradientGen(nbr1 => {
+                this.latitude = CENTER_Y + (nbr1 / NBR_GRADIENTS * AROUND_X)
+                gradientGen(nbr2 => {
+                    this.longitude = CENTER_X + (nbr2 / NBR_GRADIENTS * AROUND_X)
+                    let lat = this.latitude
+                    let lon = this.longitude
+                    if (ALLOW_GEOCODER === true)
+                        setTimeout( () => {
+                            geocoder.reverse({lat, lon}).then(call => {
+                                resolve({lati: lat, longi: lon, zip: call[0].zipcode, city: call[0].city});
+                            })
+                        }, 1000);
+                })
             })
         })
     }
 
     this.isFill = 1;
     this.popularite = getRandomInt(500);
-    this.picture = 'https://randomuser.me/api/portraits/men/'+getRandomInt(99)+'.jpg';
+    if (this.gender === 0)
+        this.picture = 'https://randomuser.me/api/portraits/men/'+getRandomInt(99)+'.jpg';
+    else
+        this.picture = 'https://randomuser.me/api/portraits/women/'+getRandomInt(99)+'.jpg';
     this.isProfile = 1;
     this.isFake = 1;
 
@@ -99,10 +116,20 @@ let Profile = function() {
         })
     };
 
-    this.getSQLProfiles = () => {
-    this.setLocation()
-    return `('${this.idAccount}', '${this.name}', '${this.firstname}', ${this.gender}, ${this.age},\
-    ${this.sexuality}, ${this.latitude}, ${this.longitude}, ${this.popularite}, ${this.isFill})`;
+    this.getSQLProfiles = (str) => {
+        return new Promise(async (resolve, reject) => {
+            await this.setLocation().then( obj =>
+            {
+                this.longitude = obj.longi
+                this.latitude = obj.lati
+                this.city = obj.city
+                this.zipcode = obj.zip
+                console.log(`Lat : ${this.latitude} - Lon : ${this.longitude}`)
+                str.s = `('${this.idAccount}', '${this.name}', '${this.firstname}', ${this.gender}, ${this.age},\
+                ${this.sexuality}, '${this.zipcode}', '${this.city}', ${this.latitude}, ${this.longitude}, ${this.popularite}, ${this.isFill})`;
+                resolve();
+            })
+        })
     };
 
     this.getSQLInterests = (nbrInterests) => {
@@ -154,12 +181,15 @@ let addInDb = (nbr) => {
 }
 
 let addInDbProfiles = (nbr) => {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         let SQLQuery = 'INSERT INTO profiles \
-        (id_account, name, firstname, gender, age, sexuality, latitude, longitude, popularite, isFill) \
+        (id_account, name, firstname, gender, age, sexuality, zipcode, city, latitude, longitude, popularite, isFill) \
         VALUES ';
         for (let i = 0; i < nbr; i++) {
-            SQLQuery += prfs[i].getSQLProfiles();
+            var str = { s: "B"};
+            await prfs[i].getSQLProfiles(str);
+            SQLQuery += str.s;
+            console.log(SQLQuery);
             if (i !== nbr - 1)
                 SQLQuery += ',';
         }
